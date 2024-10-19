172d11ab1160d0561ab8d8520c1acc08c86b5d4c
Venkat <114759422+Venkatramshesh@users.noreply.github.com>
2024-10-19 10:58:20 -0400

    Initial Commit

---
+++ Travelapp/TravelappApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelappApp.swift
+//  Travelapp
+//
+//  Created by Venkat Ramshesh on 10/19/24.
+//
+
+import SwiftUI
+
+@main
+struct TravelappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TravelappTests/TravelappTests.swift
@@ -0,0 +1,16 @@
+//
+//  TravelappTests.swift
+//  TravelappTests
+//
+//  Created by Venkat Ramshesh on 10/19/24.
+//
+
+import Testing
+
+struct TravelappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
