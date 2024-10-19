2ca1c434e0e459a70f57e0484731e8676fdc6854
Eric Rohm <rohm@MacBookAir.cougars.int>
2024-10-19 13:03:47 -0400

    Initial Commit

---
+++ SwiftUIChatDemo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUIChatDemo
+//
+//  Created by Eric Rohm on 10/19/24.
+//
+
+import SwiftUI
+import StreamChat
+import StreamChatSwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ChatChannelListView()
+}
