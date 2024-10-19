32d12df357e82bcb18c02034f5ddd702c3fa83f4
Venkat <114759422+Venkatramshesh@users.noreply.github.com>
2024-10-19 10:58:20 -0400

    Initial Commit

---
+++ Travelapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Travelapp
+//
+//  Created by Venkat Ramshesh on 10/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
