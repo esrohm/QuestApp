ee9f67481c3959b2ef0921e476bb3ebe6fddc116
Eric Rohm <rohm@MacBookAir.cougars.int>
2024-10-20 12:40:47 -0400

    Added User Profile screen

---
+++ SwiftUIChatDemo/ProfileHeader.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileHeader.swift
+//  SwiftUIChatDemo
+//
+//  Created by Eric Rohm on 10/20/24.
+//
+
+import SwiftUI
+
+struct ProfileHeader: View {
+    var body: some View {
+        Text("User Profile")
+    }
+}
---
+++ UserProfileView/UserProfileViewTests/UserProfileViewTests.swift
@@ -0,0 +1,17 @@
+//
+//  UserProfileViewTests.swift
+//  UserProfileViewTests
+//
+//  Created by Eric Rohm on 10/20/24.
+//
+
+import Testing
+@testable import UserProfileView
+
+struct UserProfileViewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
