71ad2be62c5ecc739604416d4ba0445debb3aaa4
Eric Rohm <rohm@MacBookAir.cougars.int>
2024-10-20 13:38:18 -0400

    Moved UserProfileView files

---
+++ SwiftUIChatDemo/UserProfileView/UserProfileView/UserProfileViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserProfileViewApp.swift
+//  UserProfileView
+//
+//  Created by Eric Rohm on 10/20/24.
+//
+
+import SwiftUI
+
+@main
+struct UserProfileViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
